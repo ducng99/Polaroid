4f35658fa4ad8b0b4fcaeb291d20c878c2599d08
ducng99 <tom@ducng.dev>
2022-06-04 17:31:42 +1200

    Cleanup

--- android-locale-string-mod.js
+++ android-locale-string-mod.js
@@ -1,9 +1,9 @@
-const withAppBuildGradle = require('@expo/config-plugins').withAppBuildGradle;
+const { withAppBuildGradle } = require('@expo/config-plugins');
 
-module.exports = function(config) {
+module.exports = function (config) {
     return withAppBuildGradle(config, async (props) => {
         props.modResults.contents = props.modResults.contents.replace('org.webkit:android-jsc:+', 'org.webkit:android-jsc-intl:+');
-        
+
         return props;
     })
 }
